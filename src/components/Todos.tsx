af259ed1855a6ae4518e2a9bba1f51b3f3d00f8c
Sherry <hhlsdkm@gmail.com>
2022-11-11 11:30:59 +0900

    fix css classes

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -6,7 +6,7 @@ import classes from "./Todos.module.css"
 
 const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
   return (
-    <ul className={classes.list}>
+    <ul className={classes.todos}>
       {props.items.map(item =>
         <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}/>
       )}
